3c0d7b4e6ac76f618698548c007a1683001e7eb6
jzscsaba <johanyak.csaba@nje.hu>
2024-10-04 21:40:51 +0200

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+
+namespace PathBasedAnimation
+{
+	/// Példaprogram C# kódból megvalósított útvonalkövető animációra.
+	/// Egy négyzetet mozgatunk úgy, hogy a bal felső sarok kövessen
+	/// egy archimédeszi spirálvonalat.
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		/// <summary>
+		/// Konstruktor.
+		/// </summary>
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+
+		/// <summary>
+		/// Animáció megvalósítása.
+		/// </summary>
+		/// <param name="sender">Indít nyomógomb.</param>
+		/// <param name="e"></param>
+		private void btIndít_Click(object sender, RoutedEventArgs e)
+		{
+			//
+			// Az animáció és a megrajzolásra kerülő görbe útvonalának meghatározása.
+			//
+			// Útvonalgeometria objektum definiálása.
+			PathGeometry pgÚtvonal = new PathGeometry();
+			// Az útvonalgeometriát egy vagy több útvonalalak (Figure) alkotja.
+			// Útvonalalak objektum definálása.
+			PathFigure pfÚtvonal = new PathFigure();
+			// Kiindulópont definiálása.
+			pfÚtvonal.StartPoint = new Point(200, 200);
+			// Kezdetben a négyzet bal felső sarka legyen a kiindulópontban.
+			rcNégyzet.SetValue(Canvas.LeftProperty, pfÚtvonal.StartPoint.X);
+			rcNégyzet.SetValue(Canvas.TopProperty, pfÚtvonal.StartPoint.Y);
+			// A sprirált definiáló objektum létrehozása.
+			
+			var asSpirál = new ArhimédesziSpirál(pfÚtvonal.StartPoint.X, pfÚtvonal.StartPoint.Y,
+				10, 0, 1080, 5);
+			// A spirált meghatározó pontok lekérdezése (egy List<Point> objektum)+
+			// A meghatározó pontokat egy egy egyenessel kösse össze az útvonal.
+			PolyLineSegment plsVonal =
+				new PolyLineSegment((System.Collections.Generic.IEnumerable<Point>)asSpirál.Pontok, true);
+			// Szegmens hozzáadása az útvonalalakhoz.
+			pfÚtvonal.Segments.Add(plsVonal);
+			// Útvonalalak hozzáadása a geometriához.
+			pgÚtvonal.Figures.Add(pfÚtvonal);
+			// Útvonalgeometria rögzítése, ami teljesítményjavulást eredményez.
+			pgÚtvonal.Freeze();
+			//
+			// A görbe megrajzolása.
+			//
+			// Útvonal objektum definiálása.
+			Path ptVonal = new Path();
+			// A görbe útvonalának megadása.
+			ptVonal.Data = pgÚtvonal;
+			// A görbe vonalszíne.
+			ptVonal.Stroke = Brushes.Black;
+			// A görbe vonalvastagsága.
+			ptVonal.StrokeThickness = 2;
+			// Görbe megjelenítése=felvétele a Canvasra
+			cvLap.Children.Add(ptVonal);
+			//
+			// Animáció megvalósítása - 2 komponens lesz
+			//
+			// Animáció objektum definiálása a vízszintes irányú mozgatáshoz.
+			DoubleAnimationUsingPath daxAnimáció =
+					new DoubleAnimationUsingPath();
+			// Az útvonal megadása.
+			daxAnimáció.PathGeometry = pgÚtvonal;
+			// Az animáció időtartama.
+			daxAnimáció.Duration = TimeSpan.FromSeconds(10);
+			// Az animáció az x koordinátára vonatkozzon.
+			daxAnimáció.Source = PathAnimationSource.X;
+			// Csinálja meg visszafele is.
+			daxAnimáció.AutoReverse = true;
+			// Animáció objektum definiálása a függőleges irányú mozgatáshoz.
+			DoubleAnimationUsingPath dayAnimáció =
+					new DoubleAnimationUsingPath();
+			// Az útvonal megadása.
+			dayAnimáció.PathGeometry = pgÚtvonal;
+			// Az animáció időtartama.
+			dayAnimáció.Duration = TimeSpan.FromSeconds(10);
+			// Az animáció az y koordinátára vonatkozzon.
+			dayAnimáció.Source = PathAnimationSource.Y;
+			// Csinálja meg visszafele is.
+			dayAnimáció.AutoReverse = true;
+			// Animáció indítása.
+			rcNégyzet.BeginAnimation(Canvas.TopProperty, dayAnimáció);
+			rcNégyzet.BeginAnimation(Canvas.LeftProperty, daxAnimáció);
+		}
+	}
+}
---
+++ Spiral.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+
+namespace PathBasedAnimation
+{
+	/// <summary>
+	/// Archimédeszi spirál számítását támogató osztály.
+	/// </summary>
+	class ArhimédesziSpirál
+	{
+		/// <summary>
+		/// Kiindulópont x koordinátája.
+		/// </summary>
+		private double x0;
+		/// <summary>
+		/// Kiindulópont y koordinátája.
+		/// </summary>
+		private double y0;
+		/// <summary>
+		/// Együttható;
+		/// </summary>
+		private double k;
+		/// <summary>
+		/// A spirálszámítás kezdőszöge radiánban.
+		/// </summary>
+		private double KezdőSzög;
+		/// <summary>
+		/// A spirálszámítás zárószöge radiánban.
+		/// </summary>
+		private double ZáróSzög;
+		/// <summary>
+		/// Két egymásutáni szögérték távolsága radiánban.
+		/// </summary>
+		private double Lépés;
+		/// <summary>
+		/// A pontokat tároló lista objektum.
+		/// </summary>
+		private List<Point> lpPontok;
+		/// <summary>
+		/// Konstruktor. Inicializálja az adatagokat.
+		/// </summary>
+		/// <param name="X0">Kiindulópont x koordinátája.</param>
+		/// <param name="Y0">Kiindulópont y koordinátája.</param>
+		/// <param name="K">Együttható;</param>
+		/// <param name="KSz">A spirálszámítás kezdőszöge fokban.</param>
+		/// <param name="ZSz">A spirálszámítás zárószöge fokban.</param>
+		/// <param name="L">Két egymásutáni szögérték távolsága fokban.</param>
+		public ArhimédesziSpirál(double X0, double Y0, double K, double KSz,
+			double ZSz, double L)
+		{
+			x0 = X0;
+			y0 = Y0;
+			k = K;
+			// A szögeket radiánba alakítjuk.
+			KezdőSzög = KSz * Math.PI / 180;
+			ZáróSzög = ZSz * Math.PI / 180;
+			Lépés = L * Math.PI / 180;
+		}
+		/// <summary>
+		/// Lehetővé teszi a görbét meghatározó pontok lekérdezését (kiszámítását).
+		/// </summary>
+		public List<Point> Pontok
+		{
+			get
+			{
+				// A pontokat tároló lista objektum létrehozása.
+				lpPontok = new List<Point>();
+				// Minden szöghöz kiszámítja a pont x,y koordinátáját.
+				for (double fi = KezdőSzög; fi <= ZáróSzög; fi += Lépés)
+				{
+					double r = k * fi;
+					lpPontok.Add(new Point(x0 + r * Math.Cos(fi), y0 - r * Math.Sin(fi)));
+				}
+				return lpPontok;
+			}
+		}
+	}
+}
